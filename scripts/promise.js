03f14471beb7cbe4eff87416e840aab9cf4539cd
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-08 09:22:55 +0530

    promise examples.

--- scripts/promise.js
+++ scripts/promise.js
@@ -1,11 +1,12 @@
 // dummy promises
 const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("P1 success"), 3000);
+//   setTimeout(() => resolve("P1 success"), 3000);
+  setTimeout(() => reject("p1 fail"), 3000);
 });
 
 const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("p2 success"), 1000);
-  // setTimeout(() => reject("p2 fail"), 1000);
+//   setTimeout(() => resolve("p2 success"), 1000);
+  setTimeout(() => reject("p2 fail"), 1000);
 });
 
 const p3 = new Promise((resolve, reject) => {
@@ -37,7 +38,7 @@ Promise.allSettled([p1, p2, p3])
   });
 
 // promise.race example
-// whatever is the first one (success/ failure) it'll return that result.
+// whatever is the first one (success/failure) it'll return that result.
 // whatever takes less time it'll be returned first.
 // the result will be the first setteled promise.
 Promise.race([p1, p2, p3])
@@ -50,11 +51,13 @@ Promise.race([p1, p2, p3])
 
 // promise.any example
 // promise.any will wait for the first resolved/settled success.
-// if all the promises fail it'll give u an aggregated error.
+// if all the promises fail it'll give u an aggregate error.
+//
 Promise.any([p1, p2, p3])
   .then((res) => {
     console.log("promise.any success ==>", res);
   })
   .catch((err) => {
     console.error("promise.any fail ==>", err);
+    console.error("promise.any fail ==>", err.errors);
   });
