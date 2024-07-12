f8624c55b07a069baa8eed2272a6e325f046873a
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-12 10:18:40 +0530

    async await comments added.

--- scripts/async-await.js
+++ scripts/async-await.js
@@ -2,16 +2,20 @@
 // resolve the promise after 5 seconds
 
 const P = new Promise((resolve, reject) => {
+  // if the timer is set for 5 secs after 5 secs this promise will be resolved.
+  // if the timer for first promis is 10 secs and second one is 5 secs both will execue at the same time.
+  // In case of promises the code execution will not stop at the promise line.
   setTimeout(() => {
     resolve("Promise resolved");
-  }, 5000);
+  }, 10000);
 });
 
 // create second promise
 const P2 = new Promise((resolve, reject) => {
+  // after 10 secs this promise will be rsolved.
   setTimeout(() => {
     resolve("Promise resolved");
-  }, 10000);
+  }, 5000);
 });
 
 // async always returns a promise
@@ -23,7 +27,7 @@ async function getData() {
   console.log("code after promise");
 
   const promValue2 = await P2;
-  console.log("promValue1", promValue2);
+  console.log("promValue2", promValue2);
   console.log("code after second promise");
 }
 
