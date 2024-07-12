508f0c4d134f4a67f127e423b6dbcac60a15412e
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-12 10:30:42 +0530

    this substition in strict mode.

--- scripts/this.js
+++ scripts/this.js
@@ -1,5 +1,6 @@
 // this value depends on how the function is called.
 
+// this in strict mode - (this substitution)
 // if the functions is called from strict mode this inside of function will return undefined.
 
 // if the function is called under non strict mode this will return window.
