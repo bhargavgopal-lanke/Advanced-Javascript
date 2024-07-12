494d2b2f1273068c48443d5e0aed76fc723d3fd5
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-12 10:29:22 +0530

    this yesterday class revision comments.

---
+++ scripts/this.js
@@ -0,0 +1,7 @@
+// this value depends on how the function is called.
+
+// if the functions is called from strict mode this inside of function will return undefined.
+
+// if the function is called under non strict mode this will return window.
+
+// in the global space this will always return the window
