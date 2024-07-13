84abf717b18789191b266b85f872dd547a2e5915
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-13 15:09:47 +0530

    this example with strict mode.

--- scripts/this.js
+++ scripts/this.js
@@ -1,6 +1,20 @@
+"use strict";
+
+
 // this value depends on how the function is called.
 
-// this in strict mode - (this substitution)
+// this in global space
+
+console.log(this);
+
+// this inside a function
+
+function x() {
+  // this in strict mode - (this substitution)
+  // the value depends on the strict / non strict mode
+  console.log(this);
+}
+
 // if the functions is called from strict mode this inside of function will return undefined.
 
 // if the function is called under non strict mode this will return window.
