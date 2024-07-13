f393604459e32193ddca932740710b9872f09c21
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-13 15:39:49 +0530

    strict mode comments added.

--- scripts/this.js
+++ scripts/this.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 // this value depends on how the function is called.
 
 // this in global space
@@ -14,6 +13,12 @@ function x() {
   // the value depends on the strict / non strict mode
   console.log(this);
 }
+
+// in strict mode if the function is called without any reference the value of this is undfined.
+x();
+
+// if we call the function like this strict mode it'll return the window object.
+window.x();
 
 // if the functions is called from strict mode this inside of function will return undefined.
 
