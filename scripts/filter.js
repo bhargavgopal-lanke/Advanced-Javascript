528b58103b7746487f5300ed39d4b4da16228cb9
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-14 19:57:41 +0530

    import changed and odd example added.

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,1 +1,11 @@
-const arr = [5, 2, 3, 1, 6];
+const newArr = [5, 2, 3, 1, 6];
+
+// filter odd values
+
+function odd(x) {
+  return x % 2;
+}
+
+const oddOutput = newArr.filter(odd);
+
+console.log("oddOutput", oddOutput);
