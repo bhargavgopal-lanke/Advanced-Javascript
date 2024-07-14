ea8da6e64819f17b64bc28f927c821dddd6bea0d
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-14 20:17:51 +0530

    code moved to map file

--- scripts/filter.js
+++ scripts/filter.js
@@ -3,9 +3,21 @@ const newArr = [5, 2, 3, 1, 6];
 // filter odd values
 
 function odd(x) {
-  return x % 2;
+  return x % 2 !== 0;
 }
 
 const oddOutput = newArr.filter(odd);
 
 console.log("oddOutput", oddOutput);
+
+// filter odd values
+
+function even(x) {
+  return x % 2 == 0;
+}
+
+const evenOutput = newArr.filter(even);
+
+console.log("evenOutput", evenOutput);
+
+
