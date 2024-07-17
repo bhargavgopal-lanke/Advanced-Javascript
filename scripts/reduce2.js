8841b33924b6d9da904722ba1518ec276d5fcb2a
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-17 10:34:19 +0530

    find the most recent date in the array example.

--- scripts/reduce2.js
+++ scripts/reduce2.js
@@ -6,6 +6,8 @@
 
 // the reduce method does not hcnage the original array
 
+// the accumulator can be any value: number, null, undefined, array, object or even a promise
+
 // clculating the item price example
 
 const items2 = [
@@ -22,3 +24,25 @@ const items2 = [
 const itemsReduce = items2.reduce((acc, curr) => acc + curr.price, 0);
 
 console.log(itemsReduce);
+
+// example to find the most recent date
+// initial value is the date value in this example
+
+const dates = [
+  "01/08/2022",
+  "02/08/2022",
+  "04/08/2022", // this is the most recent date find it
+  "04/08/2022",
+  "21/08/2022",
+  "06/08/2022",
+  "07/08/2022",
+];
+
+const datesReduce = dates.reduce((acc, curr) => {
+  if (curr > acc) {
+    acc = curr;
+  }
+  return acc;
+}, dates[0]);
+
+console.log("datesReduce", datesReduce);
