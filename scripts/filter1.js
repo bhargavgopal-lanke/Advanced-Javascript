fe9a78296e5670cf46e9667f42db56276b8c8ef1
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-18 01:03:49 +0530

    example to find the users under or equal to age 18 using filter method

--- scripts/filter1.js
+++ scripts/filter1.js
@@ -1,7 +1,9 @@
 // filter() creates a new array by filtering out the elements
 
+// example to find out the users list under or equal to 18 years age from an array
+
 const ages = [16, 17, 18, 18, 19, 20, 60];
 
-const ageFilter = ages.filter((age) => age < 18);
+const ageFilter = ages.filter((age) => age <= 18);
 
 console.log("ageFilter", ageFilter);
