bf795e9aa857774a8ee684ca6fb1bb412a1dd2b8
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2024-07-18 01:04:47 +0530

    example to find user greater than age 18 using filter method

--- scripts/filter1.js
+++ scripts/filter1.js
@@ -7,3 +7,9 @@ const ages = [16, 17, 18, 18, 19, 20, 60];
 const ageFilter = ages.filter((age) => age <= 18);
 
 console.log("ageFilter", ageFilter);
+
+// example to find out the users list greter than or equal to 18 years age from an array
+
+const ageFilter1 = ages.filter((age) => age >= 18);
+
+console.log("ageFilter1", ageFilter1);
