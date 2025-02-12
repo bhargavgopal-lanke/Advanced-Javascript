850029410d983bcd9e5f97952d9541cdd2418944
bhargavgopal-lanke <lankebhargav1996@gmail.com>
2025-02-12 10:14:31 +0530

    age less than 30 using reduce method.

--- scripts/redcuePractice.js
+++ scripts/redcuePractice.js
@@ -54,6 +54,11 @@ const rest = users.reduce((acc, curr) => {
 // Example 4
 // find the list of people whose age is less than 30
 
-const ageLessThanThirty = users.map((x) => x.age).filter((x) => x < 30);
+const ageLessThanThirty = users.reduce((acc, curr) => {
+  if(curr.age < 30) {
+    acc.push(curr.age);
+  }
+  return acc;
+}, []);
 
 console.log("rest", ageLessThanThirty);
